ca8e8adab40b3cf5b9648cc5c28cdc62e4ee1288
Vokymir <voky.consultant@gmail.com>
2025-02-26 16:01:55 +0100

    following OpenTK Tutorial on opentk.net/learn - chapter 1 done

---
+++ opentkLearn/Game.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opentkLearn
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(
+            GameWindowSettings.Default, 
+            new NativeWindowSettings() { ClientSize = (width, height), Title = title }
+            ) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ opentkLearn/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace opentkLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game(800, 600, "OpenTKLearn"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
