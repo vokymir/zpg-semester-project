911e32cfca633a454600913dc22241e931852ebb
Vokymir <voky.consultant@gmail.com>
2025-02-26 17:22:40 +0100

    chapter 1.2 HelloTriangle DONE

--- opentkLearn/Game.cs
+++ opentkLearn/Game.cs
@@ -13,9 +13,20 @@ namespace opentkLearn
 {
     class Game : GameWindow
     {
+        float[] vertices =
+        {
+            -0.5f, -0.5f, 0.0f, // bottom-left
+            0.5f, -0.5f, 0.0f, // bottom-right
+            0.0f, 0.5f, 0.0f // top
+        };
+        int VertexBufferObject;
+        int VertexArrayObject;
+        Shader shader;
         public Game(int width, int height, string title) : base(
             GameWindowSettings.Default, 
-            new NativeWindowSettings() { ClientSize = (width, height), Title = title }
+            new NativeWindowSettings() { 
+                ClientSize = (width, height), 
+                Title = title }
             ) { }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
@@ -27,5 +38,61 @@ namespace opentkLearn
                 Close();
             }
         }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            shader = new Shader("shader.vert", "shader.frag");
+
+            GL.ClearColor(0.2f,0.3f,0.3f,1.0f);
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            
+
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            shader.Use();
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            
+            shader.Dispose();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0,0, e.Width, e.Height);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0,0,Size.X,Size.Y);
+        }
+
     }
 }
